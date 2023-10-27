c497e8dbc6d10e420ec4f6c8cba6c2db1144a741
Kolesnik Vlad <fow1078@gmail.com>
2023-10-27 08:36:56 +0200

    Synchronized

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,30 +1,33 @@
 import io.ktor.client.*
+import kotlinx.coroutines.runBlocking
 import remote.CartService
 
-suspend fun main() {
-    val service = CartService.create()
+fun main() {
+    runBlocking {
+        val service = CartService.create()
 
-    // Cart Methods Use
-    val products = service.getAllProducts()
-    //  println(products)
+        // Cart Methods Use
+        val products = service.getAllProducts()
+        //  println(products)
 
-    val product = service.getASingleProduct(1)
-    //  println(product)
+        val product = service.getASingleProduct(1)
+        //  println(product)
 
-    val specificNumberOfProducts = service.getLimitedNumberOfProducts(1)
-    //  println(specificNumberOfProducts)
+        val specificNumberOfProducts = service.getLimitedNumberOfProducts(1)
+        //  println(specificNumberOfProducts)
 
-    val productsDesc = service.getProductsSortedDesc()
-    val productsAsc = service.getProductsSortedAsc()
+        val productsDesc = service.getProductsSortedDesc()
+        val productsAsc = service.getProductsSortedAsc()
 
-    val categories = service.getAllCategories()
-    val productsByCategory = service.getProductsInCategory(categories[0])
-    // println(productsByCategory)
+        val categories = service.getAllCategories()
+        val productsByCategory = service.getProductsInCategory(categories[0])
+        // println(productsByCategory)
 
-    val productsByCategories = service.getProductsInMultipleCategories(listOf(categories[0], categories[2]))
-    // println(productsByCategories)
+        val productsByCategories = service.getProductsInMultipleCategories(listOf(categories[0], categories[2]))
+        // println(productsByCategories)
 
-    val carts = service.getAllCarts()
-    val cartsByProductId = service.getAllCartsBySpecificProductId(1)
-    //  println(cartsByProductId)
+        val carts = service.getAllCarts()
+        val cartsByProductId = service.getAllCartsBySpecificProductId(1)
+        //  println(cartsByProductId)
+    }
 }
